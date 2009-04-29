df505cf52a7a3867e8bf3d432e3cbd78848794db
Michael Bleigh <michael@intridea.com>
2009-04-29 17:17:33 -0400

    Adding oauth to gem deps.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'oauth'
   config.gem 'twitter-auth', :lib => 'twitter_auth'
   config.gem 'haml', :version => '>= 2.0.6'
   config.gem 'icalendar'
